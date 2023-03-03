024eb33e0cc2e9e3ab853533964c73b4f0799766
mykfrost <okellofrost@gmail.com>
2023-03-03 13:13:48 +0300

    using a listview

--- app/src/main/java/com/chinatown254/dgasnersql/MainActivity.java
+++ app/src/main/java/com/chinatown254/dgasnersql/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import com.chinatown254.dgasnersql.model.DataItem;
 import com.chinatown254.dgasnersql.sample.SampleDataProvider;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
@@ -16,6 +17,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 TextView tv;
 List<DataItem> dataItemList = SampleDataProvider.dataItemList;
+List<String> itemNames = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,17 +25,19 @@ List<DataItem> dataItemList = SampleDataProvider.dataItemList;
 
 //        DataItem item = new DataItem(null,"my menu item", "a category",
 //                "a description",1,99.5,"apple_pie.jpg");
-        tv =  findViewById(R.id.tvout);
-        tv.setText("");
-
-        Collections.sort(dataItemList, new Comparator<DataItem>() {
-            @Override
-            public int compare(DataItem o1, DataItem o2) {
-                return o1.getItemName().compareTo(o2.getItemName());
-            }
-        });
+//        tv =  findViewById(R.id.tvout);
+//        tv.setText("");
+//
+//        Collections.sort(dataItemList, new Comparator<DataItem>() {
+//            @Override
+//            public int compare(DataItem o1, DataItem o2) {
+//                return o1.getItemName().compareTo(o2.getItemName());
+//            }
+//        });
         for (DataItem item: dataItemList) {
-            tv.append(item.getItemName() +"\n");
+//            tv.append(item.getItemName() +"\n");
+            itemNames.add(item.getItemName());
         }
+        Collections.sort(itemNames);
     }
 }
