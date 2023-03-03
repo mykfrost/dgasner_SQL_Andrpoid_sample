88c26607e3875d92c1a5bcae7a8eceb24aadc94f
mykfrost <okellofrost@gmail.com>
2023-03-03 11:54:18 +0300

    sorting data alphabetically

--- app/src/main/java/com/chinatown254/dgasnersql/MainActivity.java
+++ app/src/main/java/com/chinatown254/dgasnersql/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.TextView;
 import com.chinatown254.dgasnersql.model.DataItem;
 import com.chinatown254.dgasnersql.sample.SampleDataProvider;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,9 +21,19 @@ List<DataItem> dataItemList = SampleDataProvider.dataItemList;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DataItem item = new DataItem(null,"my menu item", "a category",
-                "a description",1,99.5,"apple_pie.jpg");
+//        DataItem item = new DataItem(null,"my menu item", "a category",
+//                "a description",1,99.5,"apple_pie.jpg");
         tv =  findViewById(R.id.tvout);
-        tv.setText(item.toString());
+        tv.setText("");
+
+        Collections.sort(dataItemList, new Comparator<DataItem>() {
+            @Override
+            public int compare(DataItem o1, DataItem o2) {
+                return o1.getItemName().compareTo(o2.getItemName());
+            }
+        });
+        for (DataItem item: dataItemList) {
+            tv.append(item.getItemName() +"\n");
+        }
     }
 }
